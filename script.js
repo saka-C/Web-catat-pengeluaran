98caafab04cb31ad05f2f650758a920b789a70d2
saka-C <shakaaoofaalhajar@gmail.com>
2023-09-08 15:34:15 +0700

    third commit

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
-
-burger.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
 
 
-const navbar = document.querySelector(".navbar");
+
 
 // scrolled
 // ================
+const navbar = document.querySelector(".navbar");
 window.addEventListener("scroll", function () {
   if (window.scrollY > 0) {
     navbar.classList.add("scrolled");
